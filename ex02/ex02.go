29d3b7e918fd921a4e598c6a4282ecf49dd54750
Lars Henrik Bolstad <lahebo@gmail.com>
2016-01-21 13:28:31 +0100

    Create ex02.go

---
+++ ex02/ex02.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	. "fmt"
+	"runtime"
+	"time"
+)
+
+var i = 0
+
+func thread1() {
+	for j := 0; j < 1000000; j++ {
+		i++
+	}
+}
+
+func thread2() {
+	for j := 0; j < 1000000; j++ {
+		i--
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	go thread1()
+	go thread2()
+
+	time.Sleep(100 * time.Millisecond)
+	Println(i)
+}
