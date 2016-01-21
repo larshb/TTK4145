3abcb6b1e00b56e638b62e4e3d136536f43b1d82
Lars Henrik Bolstad <lahebo@gmail.com>
2016-01-21 11:07:10 +0100

    Created Go-file

---
+++ ex01/ex01.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var i = 0
+
+func thread1() {
+	for j := 0; j < 1000000; j++ {
+		i++
+	}
+}
+
+func thread2() {
+	for j := 0; j < 1000000; j++ {
+		i--
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	go thread1()
+	go thread2()
+
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println(i)
+}
