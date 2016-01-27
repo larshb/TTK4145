4de0275a00f31a308b90ba1ad8e87b22672efaae
EspenMO <espenmo92@hotmail.com>
2016-01-27 10:25:09 +0100

    Update tcp.py

--- ex03/tcp.py
+++ ex03/tcp.py
@@ -4,15 +4,13 @@ import socket
 TCP_IP = '129.241.187.23'
 TCP_PORT = 33546
 BUFFER_SIZE = 1024
-MESSAGE = ''
+msg = ''
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-while MESSAGE != 'QUIT':
-  MESSAGE = raw_input('->')
-  s.send(MESSAGE + '\0')
+while msg != 'QUIT':
+  msg = raw_input('Message: ')
+  s.send(msg + '\0')
   data, addr = s.recvfrom(BUFFER_SIZE)
-  print "received data:", data, addr
-  MESSAGE = ''
-  data = ''
+  print "\nReceived: ", data
 s.close()
