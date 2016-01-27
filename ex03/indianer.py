0478a9a03bc6b5c160c6699ca8552fb5708637de
Lars Henrik Bolstad <lahebo@gmail.com>
2016-01-27 10:13:20 +0100

    Create indianer.py

---
+++ ex03/indianer.py
@@ -0,0 +1,17 @@
+from socket import *
+import time
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+i = 1
+while True:
+	data = str(i)
+	if i % 3 == 0:
+		data += ' indianere'
+	else:
+		data += ' og'
+	i += 1
+	for j in range(10000,65535):
+		s.sendto(data, ('129.241.187.255', j))
+	print 'sendt:', data
+	time.sleep(2)
