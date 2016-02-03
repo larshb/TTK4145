6a679bac3ffbcf20e61624c6a0b3ad4f5d629761
Rallstad <haraldkjorholt@gmail.com>
2016-02-03 12:04:22 +0100

    minor changes to tcp client/server

--- ex04/tcp_client.py
+++ ex04/tcp_client.py
@@ -5,17 +5,22 @@ TCP_PORT = 20022
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
+def sendPackage():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
 
-print "received data:", data
+	print "received data:", data
 
 def menu():
 	choice = ''
 	while not choice == 0:
 		print "1) Send package"
 		print "0) Exit"
-		choice = input('Send')
+		choice = input("Choose: ")
+		if choice == 1:
+			sendPackage()
+
+menu()
--- ex04/tcp_server.py
+++ ex04/tcp_server.py
@@ -8,11 +8,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print 'Connection address:', addr
 while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)
-conn.close()
+	conn, addr = s.accept()
+	print 'Connection address:', addr
+	while 1:
+	    data = conn.recv(BUFFER_SIZE)
+	    print bool(data)
+	    if not data: break
+	    print "received data:", data
+	    conn.send(data)
+	conn.close()
