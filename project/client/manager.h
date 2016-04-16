c9cf85f97e86ca20bfd875fe4950288d0c3426d7
ChristineSolberg <chr-sol@hotmail.com>
2016-04-16 23:57:27 +0200

    added functionality to transaction manager

--- project/client/manager.h
+++ project/client/manager.h
@@ -51,7 +51,7 @@ int manager_assign(int button_dir, int floor) {
 			chosen_elev_rank = curr_e->rank;
 			check = 1;			
 		}
-		else if(button_dir == 1 && curr_e->floor > floor && curr_e->direction == 1 && flr < last_floor){ //Up
+		else if(button_dir == 1 && curr_e->floor > floor && curr_e->direction == 1 && flr < last_floor){ //Down
 			last_floor = flr;
 			chosen_elev_rank = curr_e->rank;
 			check = 1;			
@@ -61,6 +61,30 @@ int manager_assign(int button_dir, int floor) {
 		return chosen_elev_rank;
 	}
 
+
+	//Check different direction from end
+	int end_floor = button_dir == 0 ? 0 : N_FLOORS
+
+
+	for (int elev_id = 0; elev_id < MAX_ELEVATORS; elev_id++) {
+		curr_e = &remote_elevator[elev_id];
+		flr = abs(end_floor - floor);
+		if (!curr_e->active)
+			break;
+		if(button_dir == 0 && curr_e->direction == 1 && flr < end_floor){ //Up
+			last_floor = flr;
+			chosen_elev_rank = curr_e->rank;
+			check = 1;			
+		}
+		else if(button_dir == 1 && curr_e->direction == 0 && flr < end_floor){ //Down
+			last_floor = flr;
+			chosen_elev_rank = curr_e->rank;
+			check = 1;			
+		}
+	}
+	if(check == 1){
+		return chosen_elev_rank;
+	}
 
 
 
